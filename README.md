8fd148d823bfd37e4db2432a8c1d39c3ee944d8e
John Olatubosun <toluolatubosun@gmail.com>
2022-03-23 14:00:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Full Battery Notification with Python
+
+![Python Logo](https://www.python.org/static/img/python-logo@2x.png "Python Logo").
+
+This is a simple python script that notifies you when your battery is fully charged
+
+The script checks if your battery is plugged in and fully charged every 30 minutes and when it is fully charged it reminds you to unplug your device every 2 minutes
---
+++ full_battery.py
@@ -0,0 +1,18 @@
+import psutil
+import pyttsx3
+import time
+
+while True:
+    sleep_time = 1800
+    battery = psutil.sensors_battery()
+
+    # Laptop is plugged and full
+    if battery.power_plugged and battery.percent == 100:
+        engine = pyttsx3.init()
+        engine.say("Battery is full, unplug the charger")
+        engine.runAndWait()
+        sleep_time = 120
+    
+    time.sleep(sleep_time)
+
+
