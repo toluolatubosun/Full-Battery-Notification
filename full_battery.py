f4e376364bb7f3aaed3825da826766e9b1d55d5d
John Olatubosun <toluolatubosun@gmail.com>
2022-05-06 23:18:05 +0100

    chore: created function for speech and adjusted sleep timer

--- full_battery.py
+++ full_battery.py
@@ -2,6 +2,11 @@ import psutil
 import pyttsx3
 import time
 
+def say(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
 while True:
     sleep_time = 600
     battery = psutil.sensors_battery()
@@ -10,10 +15,8 @@ while True:
     if battery.power_plugged and battery.percent == 100:
         # Alerts you three times
         for i in range(0, 3):
-            engine = pyttsx3.init()
-            engine.say("Battery is full, unplug the charger")
-            engine.runAndWait()
-        sleep_time = 120
+            say("Full battery")
+        sleep_time = 60
 
     print(f"Running... \nBattery: {battery.percent}%\n")
     time.sleep(sleep_time)
